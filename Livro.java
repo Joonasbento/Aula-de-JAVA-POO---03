64763ea7e644ca48eb682e5c1bc9edc25e71ce06
Joonasbento <148402233+Joonasbento@users.noreply.github.com>
2025-02-14 22:37:18 +0000

    Aula03

---
+++ Livro.java
@@ -0,0 +1,20 @@
+public class Livro {
+
+
+    String titulo;
+    String autor;
+    String numeroDePaginas;
+    String preço;
+    String isbm;
+    
+
+    //Cronstrutor
+    
+
+
+
+
+
+
+    
+}
