6de4a0667ecb28ccd752cc27506866bbaa232eed
Joonasbento <148402233+Joonasbento@users.noreply.github.com>
2025-02-27 23:05:19 +0000

    Salvar

--- ProjetoSenai.java
+++
@@ -1,8 +0,0 @@
-public class ProjetoSenai {
-    
-
-    public static void main(String[] args) {
-        System.out.println("Jonas.");
-        
-    }
-}
--- QuestãoClientePet/MainClientePet.java
+++
@@ -1,6 +0,0 @@
-public class MainClientePet {
-
-   Cliente cliente
-
-
-}
